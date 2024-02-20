20ea5b2a7ae3f14af2c4c742a97b5c3cad190ee3
Shaon Bhattacharjee <bshaon19@gmail.com>
2024-02-20 16:57:21 +0530

    v3

--- pages/components/AssesmentDashboard/Assessment.jsx
+++ pages/components/AssesmentDashboard/Assessment.jsx
@@ -18,8 +18,7 @@ import Switch from "@mui/material/Switch";
 import InfoIcon from "@mui/icons-material/Info";
 import { css } from "styled-components";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
-import { Draggable, Droppable } from "react-beautiful-dnd";
-import { DragDropContext } from "react-beautiful-dnd";
+import TextField from "@mui/material/TextField";
 
 const ContainerGrid = styled(Box)`
   display: grid;
@@ -122,6 +121,7 @@ const InfoContainer = styled(Box)`
 `;
 
 const InfoHelpTest = styled(Box)`
+  z-index: 500;
   font-size: 10px;
   color: #9e9e9e;
   padding: 4px;
@@ -130,19 +130,28 @@ const InfoHelpTest = styled(Box)`
   top: 3px;
   background-color: #ffffff;
   border-radius: 4px 4px 0 4px;
-
-  z-index: 999;
 `;
 
-const steps = [
-  "Select master blaster campaign settings",
-  "Create an ad group",
-  "Create an ad",
-];
+const SaveCustomList = styled(Box)`
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 420px;
+  background-color: gray;
+  position: absolute;
+  border-radius: 8px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 const Helptext =
   'The Assessment Module interface allows users to select and arrange assessment module from . Once organized in the "Selected Assessment Modules" section, users can add them to a customized list called the "Custom Assessment List.';
 
+////COMPONENT
 const Assessment = () => {
   const Listone = [
     "Assessment 1",
@@ -150,7 +159,6 @@ const Assessment = () => {
     "Assessment 3",
     "Assessment 4",
   ];
-  const Listtwo = ["Assessment 3"];
 
   const [showInfoHelpTest1, setShowInfoHelpTest1] = useState(false);
   const [showInfoHelpTest2, setShowInfoHelpTest2] = useState(false);
@@ -159,7 +167,9 @@ const Assessment = () => {
   const [editOn, setEditOn] = React.useState(false);
   const [allAssessmentModules, setAllAssessmentModules] = useState(Listone);
   const [selectedModules, setSelectedModules] = useState([]);
-  const [customAssessmentList, setCustomAssessmentList] = useState(Listtwo);
+  const [customAssessmentList, setCustomAssessmentList] = useState([]);
+  const [customListName, setCustomListName] = useState([]);
+  const [isOpenSave, setIsOpenSave] = useState(false);
 
   ///// Darg&drop Handler functions
   const dargStarted = (e, item) => {
@@ -218,8 +228,60 @@ const Assessment = () => {
     setCustomAssessmentList(updatedArray);
   };
 
+  ////Save Custom Assesment
+
+  const openSaveBox = () => {
+    setIsOpenSave((prev) => !prev);
+  };
+
+  const saveCustomAssesment = () => {
+    let customList = { name: customListName };
+    selectedModules.map((data, idx) => {
+      customList = { ...customList, [idx + 1]: data };
+    });
+    setCustomAssessmentList((prev) => [...prev, customList]);
+
+    console.log(customList);
+  };
+
   return (
-    <DragDropContext>
+    <>
+      {/* ASSESMENT SAVE  */}
+      {isOpenSave && (
+        <SaveCustomList>
+          <TextField
+            id="filled-basic"
+            label="Filled"
+            variant="filled"
+            sx={{ width: "70%" }}
+            onChange={(eve) => {
+              setCustomListName(eve.target.value);
+            }}
+          />
+
+          <Button
+            variant="contained"
+            sx={{ width: "20%" }}
+            onClick={() => {
+              saveCustomAssesment();
+            }}
+          >
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            sx={{ width: "20%" }}
+            onClick={() => {
+              openSaveBox();
+            }}
+          >
+            cancel
+          </Button>
+        </SaveCustomList>
+      )}
+
+      {/* EDIT MODE  */}
       <EditContainer>
         {
           <EditHelper editOn={editOn}>
@@ -243,119 +305,106 @@ const Assessment = () => {
       </EditContainer>
 
       <ContainerGrid>
-        {/* /////////////////////////GRID 1/////////////////////////*/}
+        {/* ///////////GRID 1/////////*/}
+        <div>
+          <Header>All Assessment Modules</Header>
+          <CustomList>
+            {showInfoHelpTest1 && (
+              <InfoHelpTest>this is help text 1</InfoHelpTest>
+            )}
+            <InfoContainer
+              onMouseEnter={(eve) => helpTextHandler(eve, "section1")}
+              onMouseLeave={(eve) => helpTextHandler(eve)}
+            >
+              <InfoIcon sx={{ color: "#B6B2B2" }} />
+            </InfoContainer>
+            {allAssessmentModules.map((value, index) => {
+              const labelId = `label-${value}`;
 
-        <Droppable droppableId="AllAssessment">
-          {(provided) => (
-            <div>
-              <Header>All Assessment Modules</Header>
-
-              <CustomList ref={provided.innerRef} {...provided.droppableProps}>
-                {showInfoHelpTest1 && (
-                  <InfoHelpTest>this is help text 1</InfoHelpTest>
-                )}
-                <InfoContainer
-                  onMouseEnter={(eve) => helpTextHandler(eve, "section1")}
-                  onMouseLeave={(eve) => helpTextHandler(eve)}
-                >
-                  <InfoIcon sx={{ color: "#B6B2B2" }} />
-                </InfoContainer>
-                {allAssessmentModules.map((value, index) => {
-                  const labelId = `label-${value}`;
-
-                  return (
-                    <Draggable
-                      key={labelId}
-                      index={index}
-                      draggableId={"kk" + index}
-                    >
-                      {(provided) => (
-                        <CustomListItems>
-                          <ListItemButton
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            role={undefined}
-                            dense
-                          >
-                            <ListItemIcon>
-                              <ViewStreamIcon />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={value} />
-                          </ListItemButton>
-                        </CustomListItems>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </CustomList>
-            </div>
-          )}
-        </Droppable>
+              return (
+                <React.Fragment key={value}>
+                  {/* Dragable  */}
+                  <CustomListItems
+                    editOn={editOn}
+                    draggable
+                    onDragStart={(e) => dargStarted(e, value)} //props
+                  >
+                    <ListItemButton role={undefined} dense>
+                      <ListItemIcon>
+                        <ViewStreamIcon />
+                      </ListItemIcon>
+                      <ListItemText id={labelId} primary={value} />
+                    </ListItemButton>
+                  </CustomListItems>
+                  {index !== Listone.length - 1}
+                </React.Fragment>
+              );
+            })}
+          </CustomList>
+        </div>
 
         {/* /////////////////////////GRID 2/////////////////////////*/}
-
         <div>
           <Header>Selected Assessment Modules</Header>
 
-          {/*  droppable*/}
-
-          <Droppable droppableId="SelectedAssesment">
-            {(provided) => (
-              <CustomList {...provided.droppableProps} ref={provided.innerRef}>
-                {showInfoHelpTest2 && (
-                  <InfoHelpTest>this is help text for grid 2</InfoHelpTest>
-                )}
-
-                <InfoContainer
-                  onMouseEnter={(eve) => helpTextHandler(eve, "section2")}
-                  onMouseLeave={(eve) => helpTextHandler(eve)}
-                >
-                  <InfoIcon sx={{ color: "#B6B2B2" }} />
-                </InfoContainer>
-
-                {selectedModules.map((value, index) => {
-                  const labelId = `checkbox-list-label-${value}`;
-
-                  return (
-                    <React.Fragment key={value}>
-                      <CustomListItems editOn={editOn}>
-                        <ListItemButton role={undefined} dense>
-                          <ListItemIcon>
-                            <CheckBoxIcon />
-                          </ListItemIcon>
-                          <ListItemText id={labelId} primary={value} />
-                        </ListItemButton>
-                        {editOn && (
-                          <RemoveCircleIcon
-                            sx={{ color: "#d93e33", cursor: "pointer" }}
-                            onClick={() => deleteSelectedAssessment(value)}
-                          />
-                        )}
-                      </CustomListItems>
-                      {index !== Listone.length - 1}
-                    </React.Fragment>
-                  );
-                })}
-
-                {/* {editOn && selectedModules.length !== 0 && (
-                  <Fab
-                    color="primary"
-                    aria-label="add"
-                    sx={{
-                      height: "40px",
-                      width: "40px",
-                      marginRight: "8px",
-                      float: "right",
-                    }}
-                  >
-                    <AddIcon />
-                  </Fab>
-                )} */}
-              </CustomList>
+          <CustomList
+            droppable
+            onDragOver={(e) => {
+              dragOver(e);
+            }}
+            onDrop={(e) => dragDroped(e)}
+          >
+            {showInfoHelpTest2 && (
+              <InfoHelpTest>this is help text for grid 2</InfoHelpTest>
             )}
-          </Droppable>
+            <InfoContainer
+              onMouseEnter={(eve) => helpTextHandler(eve, "section2")}
+              onMouseLeave={(eve) => helpTextHandler(eve)}
+            >
+              <InfoIcon sx={{ color: "#B6B2B2" }} />
+            </InfoContainer>
+
+            {selectedModules.map((value, index) => {
+              const labelId = `checkbox-list-label-${value}`;
+
+              return (
+                <React.Fragment key={value}>
+                  {/*  droppable*/}
+                  <CustomListItems editOn={editOn}>
+                    <ListItemButton role={undefined} dense>
+                      <ListItemIcon>
+                        <CheckBoxIcon />
+                      </ListItemIcon>
+                      <ListItemText id={labelId} primary={value} />
+                    </ListItemButton>
+                    {editOn && (
+                      <RemoveCircleIcon
+                        sx={{ color: "#d93e33", cursor: "pointer" }}
+                        onClick={() => deleteSelectedAssessment(value)}
+                      />
+                    )}
+                  </CustomListItems>
+                  {index !== Listone.length - 1}
+                </React.Fragment>
+              );
+            })}
+
+            {editOn && selectedModules.length !== 0 && (
+              <Fab
+                color="primary"
+                aria-label="add"
+                sx={{
+                  zIndex: "1",
+                  height: "40px",
+                  width: "40px",
+                  marginRight: "8px",
+                  float: "right",
+                }}
+              >
+                <AddIcon onClick={() => openSaveBox()} />
+              </Fab>
+            )}
+          </CustomList>
         </div>
 
         {/* /////////////////////////GRID 3/////////////////////////*/}
@@ -381,7 +430,7 @@ const Assessment = () => {
                       <ListItemIcon>
                         <CreateNewFolderIcon />
                       </ListItemIcon>
-                      <ListItemText id={labelId} primary={value} />
+                      <ListItemText id={labelId} primary={value.name} />
                     </ListItemButton>
 
                     {editOn && (
@@ -400,7 +449,7 @@ const Assessment = () => {
           </CustomList>
         </div>
       </ContainerGrid>
-    </DragDropContext>
+    </>
   );
 };
 
